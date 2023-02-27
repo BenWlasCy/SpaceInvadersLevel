06057209970c1d2826edd9e661b4cd6cf3e15de9
Benjamin Wlasichuk <bwlasichuk1@nait.ca>
2023-02-27 02:04:27 -0700

    LevelOneStart

--- Assets/Scripts/GameStartBanner.cs
+++ Assets/Scripts/GameStartBanner.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class GameStartBanner : MonoBehaviour
 {
+
+    [SerializeField] GameObject levelPanel;
+    [SerializeField] float timeToStartGame;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Time.timeScale = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeToStartGame -= Time.fixedDeltaTime;
+        if (timeToStartGame <= 0)
+        {
+            StartGame();
+            timeToStartGame = 0;
+        }
+    }
+    public void StartGame()
+    {
+        Time.timeScale = 1;
+        levelPanel.SetActive(false);
     }
 }
